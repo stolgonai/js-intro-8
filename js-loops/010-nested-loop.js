0a48c975d975f82bf717cced49e0d320c7b1f463
stolgonai <asunur2009@gmail.com>
2023-11-05 13:28:16 -0600

    added forEach method

---
+++ js-loops/010-nested-loop.js
@@ -0,0 +1,15 @@
+console.log("\n ---------------NEsted Arrays -----------");
+
+for (let i = 1; i <= 3; i++) {
+  console.log(i);
+  for (let i = 1; i <= 5; i++) {
+    console.log(i);
+  }
+}
+
+for (let i = 1; i <= 3; i++) {
+  console.log(i);
+  for (let j = 1; j <= 5; j++) {
+    console.log(j);
+  }
+}
---
+++ js-loops/011-array-foreach-method.js
@@ -0,0 +1,9 @@
+const numbers = [2, 4, 6, 7];
+//multiply each el with 3 => [9,12,18,21]
+
+numbers.forEach((num) => console.log(num * 3));
+
+//Print each name
+const names = ["John", "Max", "Alina"];
+
+names.forEach((name = console.log(name)));
