6127220e8c4aa31758d7da5ea63a1d6d0e21268c
stolgonai <asunur2009@gmail.com>
2023-11-12 10:33:18 -0600

    fixed homework with objects

--- homework/005-obj-iteration-task.js
+++ homework/005-obj-iteration-task.js
@@ -32,31 +32,46 @@ const appleStore = [
 // Find the product with the smallest quantity  -> MacBook Pro 16-inch
 // Find all the product names   -> [ 'iPhone 14 Pro', 'MacBook Pro 16-inch', 'iPad Air', 'Apple Watch Series 7', 'AirPods Pro' ]
 
-function getProductDetail(arr) {
-  let mostExpensive = [];
-  let biggestQuanity = [];
-  let allProducts = [];
+// function getProductDetail(arr) {
+//   let prices = [];
+//   let quanity = [];
+//   let allProducts = [];
 
-  for (const product of appleStore) {
-    mostExpensive.push(product.price);
-    biggestQuanity.push(product.quantity);
-    allProducts.push(product.productName);
-  }
-  console.log(mostExpensive);
-  console.log(biggestQuanity);
-  return `The most expensive product is ${Math.max(...mostExpensive)}. \n
-      The least expensive product is ${Math.min(...mostExpensive)}. \n
-      The product with the biggest quantity is ${Math.max(
-        ...biggestQuanity
-      )}. \n
-      The product with the smallest quantity is ${Math.min(
-        ...biggestQuanity
-      )}. \n
-     And list of all product names are ${allProducts}. \n`;
+//   for (const product of appleStore) {
+//     prices.push(product.price);
+//     quanity.push(product.quantity);
+//     allProducts.push(product.productName);
+//   }
+
+//   // console.log(prices);
+//   // console.log(quanity);
+//   return `The most expensive product is ${Math.max(
+//     ...prices
+//   )}.\n The least expensive product is ${Math.min(
+//     ...prices
+//   )}.\n The product with the biggest quantity is ${Math.max(
+//     ...quanity
+//   )}.\n The product with the smallest quantity is ${Math.min(
+//     ...quanity
+//   )}.\n And list of all product names are ${allProducts}. \n`;
+// }
+
+let mostExpProduct = appleStore[0];
+let cheapestExpProduct = appleStore[0];
+let biggestQuanity = appleStore[0];
+let lessQuanity = appleStore[0];
+let allProducts = [];
+
+for (const product of appleStore) {
+  allProducts.push(product.productName);
+  if (product.price > mostExpProduct.price) mostExpProduct = product;
+  if (product.price < cheapestExpProduct.price) cheapestExpProduct = product;
+  if (product.quantity > mostExpProduct.quantity) biggestQuanity = product;
+  if (product.quantity > lessQuanity.quantity) lessQuanity = product;
 }
 
-console.log(getProductDetail(appleStore));
-
-// for (let i = 0; i < appleStore.length; i++) {
-//   console.log(Object.values(appleStore[i]));
-// }
+console.log(biggestQuanity.productName, "biggestQuanity");
+console.log(lessQuanity.productName, "lessQuanity");
+console.log(mostExpProduct.productName, "mostExp");
+console.log(cheapestExpProduct.productName, "cheapestExp");
+console.log(allProducts, "cheapestExp");
