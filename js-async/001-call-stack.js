13ebc8cf669c1e2a01beef4f69d99b999f770693
stolgonai <asunur2009@gmail.com>
2023-11-26 10:10:48 -0600

    dded asynch basics

---
+++ js-async/001-call-stack.js
@@ -0,0 +1,10 @@
+//primitive var
+let name = "John";
+
+//Reference types =
+const nums = [1, 2, 3];
+
+function printname() {
+  console.log("1");
+}
+print();
---
+++ js-async/002-asynchronous-js.js
@@ -0,0 +1,12 @@
+function print1() {
+  console.log("1");
+}
+function print2() {
+  console.log("2");
+}
+function print3() {
+  console.log(3);
+}
+print1();
+print2();
+print3();
