50ba24d7a2aa8bfe81b34538d467201d38fb80dd
stolgonai <asunur2009@gmail.com>
2023-11-27 20:09:37 -0600

    added async await

--- js-async/007-js-recap.js
+++ js-async/007-js-recap.js
@@ -80,3 +80,92 @@ function test1() {
 }
 
 test1();
+
+// fixing the issue with asynch-await
+function goToURL() {
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      if (Math.floor(Math.random() * 2)) resolve("URL is loaded!");
+      else reject("Website is broken!");
+    }, 2000);
+  });
+}
+
+function validateMainHeading() {
+  const actualText = "Looking to pursue a new career?"; // You will learn how to get this later
+  const expectedText = "Looking to pursue a new career?"; // Given in the requirements
+  if (expectedText === actualText) console.log("Validation is SUCCESSFUL!");
+  else console.log("Validation is FAILED!");
+}
+
+async function test1() {
+  try {
+    await goToURL();
+    validateMainHeading();
+  } catch (err) {
+    console.log * ("Error: ", err);
+  }
+}
+
+test1();
+
+function goToCostco() {
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      if (true) resolve();
+      else reject();
+    }, 2000);
+  });
+}
+
+function getMeat() {
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      if (true) resolve();
+      else reject("Could not get the meat, Costco was closed!");
+    }, 1500);
+  });
+}
+
+function cook() {
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      if (true) resolve();
+      else reject("Cooking did not go well!");
+    }, 3000);
+  });
+}
+
+function serve() {
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      if (true) resolve();
+      else reject();
+    }, 1000);
+  });
+}
+
+function eat() {
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      if (true) resolve("");
+      else reject();
+    }, 500);
+  });
+}
+
+//
+async function test() {
+  try {
+    await goToCostco();
+    await getMeat();
+    await cook();
+    await serve();
+    await eat();
+    console.log("YAYYY - PARTY WAS GOOD!");
+  } catch (err) {
+    console.log(err);
+  }
+}
+
+test();
